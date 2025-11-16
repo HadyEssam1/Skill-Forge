1d0d570c1e81fbc645c48e1764b7e7e86edd61ae
fatymali007 <fatimali120507@gmail.com>
2025-11-16 07:41:46 -0800

    more edits

--- Skill Forge/src/models/Instructor.java
+++ Skill Forge/src/models/Instructor.java
@@ -3,13 +3,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Instructor extends User{
-     private List<String> coursesTeaching;
+     private List<Course> coursesTeaching;
 
      public Instructor(int id, String username, String pass, String email) {
         super("instructor", id, username, pass, email);
         this.coursesTeaching = new ArrayList<>();
     }
-    public List<String> getCoursesTeaching(){return new ArrayList<>(coursesTeaching);}
+    public List<Course> getCoursesTeaching(){return new ArrayList<>(coursesTeaching);}
 
     public void assignCourse(String course) {
         if (!coursesTeaching.contains(course)) {
--- Skill Forge/src/models/Student.java
+++ Skill Forge/src/models/Student.java
@@ -2,13 +2,13 @@ package models;
 import java.util.List;
 import java.util.ArrayList;
 public class Student extends User {
-    private List<String> coursesEnrolled;
+    private List<Course> coursesEnrolled;
 
     public Student(int id, String username, String pass, String email, String gradeLevel) {
         super("student", id, username, pass, email);
         this.coursesEnrolled = new ArrayList<>();
     }
-    public List<String> getCoursesEnrolled() { return new ArrayList<>(coursesEnrolled); }
+    public List<Course> getCoursesEnrolled() { return new ArrayList<>(coursesEnrolled); }
 
     public void addCourse(String course) {
         if (!coursesEnrolled.contains(course)){
